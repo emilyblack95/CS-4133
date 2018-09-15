eb953e61f5450f1285fa48f2992284fcd2511402
emilyblack95 <emilycblack95@gmail.com>
2018-09-15 03:39:32 -0500

    Added completed project 1

---
+++ Project 1/UDP_Client.c
@@ -0,0 +1,99 @@
+/*
+ * UDP_Client.c
+ *
+ * Created on: Sep 2, 2018
+ * Author: emilyblack95
+ *
+ * API: https://www.tcpdump.org/manpages/pcap.3pcap.html
+ * Resource: http://www.tcpdump.org/sniffex.c
+ * Resource: http://inst.eecs.berkeley.edu/~ee122/fa07/projects/p2files/packet_parser.c
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "./pcap.h"
+
+/* port num */
+#define PORT 8080
+
+/* max size of buffer */
+#define MAXLINE 1024
+
+/* Driver */
+int main(int argc, char *argv[]) {
+	pcap_t *pcap; /* pcap file */
+	int sock, n; /* socket file descriptor */
+	int count = 1;
+	socklen_t len; /* holds size of server addr */
+
+	char buffer[MAXLINE];
+	char errbuffer[PCAP_ERRBUF_SIZE];
+	char *hello = "The client says hello.";
+
+	struct sockaddr_in servaddr; /* server address */
+	struct pcap_pkthdr header; /* The packet header */
+	const u_char *packet;
+
+	/* Skip over the program name. */
+	++argv; --argc;
+
+	/* If the name of the pcap file doesn't exist on the payload */
+	if(argc != 1) {
+		printf("%s\n", "Error: Program requires 1 argument but received no arguments.");
+		return 1;
+	}
+
+	/* Create socket */
+	if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("\nError: File descriptor not received.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Set value of servaddr */
+	memset(&servaddr, 0, sizeof(servaddr));
+
+	/* Filling server info */
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(PORT);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+
+	len = sizeof(servaddr);
+
+	/* send hello to server */
+	sendto(sock, (const char *)hello, strlen(hello), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+
+	printf("%s\n", "Success: Hello message sent.");
+
+	/* receive message, return length of message in bytes */
+	n = recvfrom(sock, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+	buffer[n] = '\0'; // null
+
+	printf("Server: %s\n", buffer);
+
+	/* Get pcap file from argv, open it */
+	pcap = pcap_open_offline(argv[0], errbuffer);
+
+	if(pcap == NULL) {
+		printf("%s\n", "Error: Error reading PCAP file.");
+		return 1;
+	}
+
+	/* Now just loop through extracting packets as long as we have some to read. */
+	while ((packet = pcap_next(pcap, &header)) != NULL) {
+		sendto(sock, (char *)packet, header.len, 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+		printf("Success: Packet #%d sent.\n", count);
+		count++;
+	}
+
+	pcap_close(pcap);
+	close(sock);
+	return 0;
+}
---
+++ Project 1/UDP_Server.c
@@ -0,0 +1,178 @@
+/*
+ * UDP_Server.c
+ *
+ * Created on: Sep 2, 2018
+ * Author: emilyblack95
+ *
+ * API: https://www.tcpdump.org/manpages/pcap.3pcap.html
+ * Resource: http://www.tcpdump.org/sniffex.c
+ * Resource: http://inst.eecs.berkeley.edu/~ee122/fa07/projects/p2files/packet_parser.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+#include "./pcap.h"
+
+/* port num */
+#define PORT 8080
+
+/* max size of buffer */
+#define MAXLINE 1024
+
+/* default snap length (maximum bytes per packet to capture) */
+#define SNAP_LEN 1518
+
+/* ethernet headers are always exactly 14 bytes [1] */
+#define SIZE_ETHERNET 14
+
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN	6
+
+/* Ethernet header */
+/* Source: http://www.tcpdump.org/sniffex.c */
+struct sniff_ethernet {
+        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
+        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
+        u_short ether_type;                     /* IP? ARP? RARP? etc */
+};
+
+/* IP header */
+/* Source: http://www.tcpdump.org/sniffex.c */
+struct sniff_ip {
+        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_char  ip_tos;                 /* type of service */
+        u_short ip_len;                 /* total length */
+        u_short ip_id;                  /* identification */
+        u_short ip_off;                 /* fragment offset field */
+        #define IP_RF 0x8000            /* reserved fragment flag */
+        #define IP_DF 0x4000            /* dont fragment flag */
+        #define IP_MF 0x2000            /* more fragments flag */
+        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+        u_char  ip_ttl;                 /* time to live */
+        u_char  ip_p;                   /* protocol */
+        u_short ip_sum;                 /* checksum */
+        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+
+/*
+ * Parses the packet and prints its data.
+ * Source: http://www.tcpdump.org/sniffex.c
+ */
+void parsePacket(int len, const u_char *packet) {
+	static int count = 1; /* packet counter */
+
+	/* declare pointers to packet headers */
+	const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
+	const struct sniff_ip *ip; /* The IP header */
+
+	int size_ip;
+
+	printf("\nPacket #%d:\n", count);
+	count++;
+
+	/* define ethernet header */
+	ethernet = (struct sniff_ethernet*)(packet);
+
+	/* define/compute ip header offset */
+	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+	size_ip = IP_HL(ip)*4;
+	if (size_ip < 20) {
+		printf("   * Invalid IP header length: %u bytes\n", size_ip);
+		return;
+	}
+
+	/* print ether header data */
+	printf("------ Ether Header ------\n");
+	printf(" Packet Size: %d bytes\n", len); /* len of entire packet */
+	printf(" Source: %02X-%02X-%02X-%02X-%02X-%02X\n", ethernet->ether_shost[0], ethernet->ether_shost[1], ethernet->ether_shost[2],
+	ethernet->ether_shost[3], ethernet->ether_shost[4], ethernet->ether_shost[5]);
+	printf(" Destination: %02X-%02X-%02X-%02X-%02X-%02X\n", ethernet->ether_dhost[0], ethernet->ether_dhost[1], ethernet->ether_dhost[2],
+	ethernet->ether_dhost[3], ethernet->ether_dhost[4], ethernet->ether_dhost[5]);
+	printf(" Ethertype: %hu\n", ntohs(ethernet->ether_type));
+
+	/* print ip header data */
+	printf("------ IP Header ------\n");
+	printf(" Version: %d\n", IP_V(ip));
+	printf(" Header length: %d bytes\n", IP_HL(ip)*IP_V(ip)); /* len of just this header */
+	printf(" Type of Service: %d\n", ip->ip_tos);
+	printf(" Total Length: %hu octets\n", ntohs(ip->ip_len));
+	printf(" Identification: %d\n", ntohs(ip->ip_id));
+	printf(" Fragment Offset: %d bytes\n", ntohs(ip->ip_off)); //Gives correct value, check https://stackoverflow.com/questions/2307531/error-parsing-ip-header
+	printf(" Time to Live: %hhu seconds/hops\n", ip->ip_ttl);
+	printf(" Protocol: %hhu\n", ip->ip_p);
+	printf(" Header Checksum: %.4x\n", ntohs(ip->ip_sum));
+	printf(" Source Address: %s\n", inet_ntoa(ip->ip_src));
+	printf(" Destination Address: %s\n", inet_ntoa(ip->ip_dst));
+	return;
+}
+
+/* Driver */
+int main() {
+	u_char packet[MAXLINE]; /* packet received from client */
+	int sock, n; /* socket file descriptor */
+	socklen_t len; /* holds size of server addr */
+
+	char buffer[MAXLINE];
+	char errbuffer[PCAP_ERRBUF_SIZE];
+	char *hello = "The server says hello.";
+
+	struct sockaddr_in servaddr; /* server address */
+	struct sockaddr_in cliaddr;
+
+	/* Create socket */
+	if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("\nError: File descriptor not received.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Set value of servaddr/cliaddr */
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+
+	/* Filling server info */
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+
+	/* Bind the socket with the server addr */
+	if(bind(sock, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+		perror("\nError: Failed to bind socket with the server address.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	len = sizeof(servaddr);
+
+	/* receive message from client, return length of message in bytes */
+	n = recvfrom(sock, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
+	buffer[n] = '\0'; // null
+
+	printf("Client: %s\n", buffer);
+
+	/* send hello to client */
+	sendto(sock, (const char *)hello, strlen(hello), 0, (const struct sockaddr *) &cliaddr, len);
+
+	printf("Success: Hello message sent.\n");
+
+	/* receive packet from client, return length of message in bytes */
+	n = recvfrom(sock, (u_char *)packet, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
+	packet[n] = '\0'; // null
+	parsePacket(n, packet);
+
+  /* receive packet from client, return length of message in bytes */
+	n = recvfrom(sock, (u_char *)packet, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
+	packet[n] = '\0'; // null
+	parsePacket(n, packet);
+
+	close(sock);
+	return 0;
+}
